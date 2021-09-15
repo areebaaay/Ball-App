2b4b3ddbf1dc1de51fad99a7fea87510c7c7fb45
Areeba Akhtar <areeba.akhtar776@gmail.com>
2021-09-16 01:18:55 +0500

    user routes added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import express from "express";
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+
 
 dotenv.config();
 
@@ -15,6 +19,12 @@ if (process.env.NODE_ENV === 'development') {
   }
 
   app.use(express.json()); 
+
+  app.use('/api/users', userRoutes);
+
+//MIDDLEWARE
+app.use(notFound);
+app.use(errorHandler);
   
 
   const PORT = process.env.PORT || 5000;
