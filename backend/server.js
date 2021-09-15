dea08bc646cd568f1fba0a9ed831d02e64bf17c1
Areeba Akhtar <areeba.akhtar776@gmail.com>
2021-09-15 23:41:26 +0500

    db config added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,29 @@
 
 import express from "express";
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+dotenv.config();
+
+connectDB();
 
 const app = express()
 
-app.listen(5000 , console.log(`Server in running on port 5000`))
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
+
+  app.use(express.json()); 
+  
+
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(
+    PORT,
+    console.log(
+      `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+  );
+
+// app.listen(5000 , console.log(`Server in running on port 5000`))
