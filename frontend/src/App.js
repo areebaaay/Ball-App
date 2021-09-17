d19ff3066e90d49db42cde857adc1c792a233e64
Areeba Akhtar <areeba.akhtar776@gmail.com>
2021-09-17 22:43:01 +0500

    modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 import Header from './components/Header';
 import { ThemeProvider } from '@material-ui/styles';
 import theme from './components/Theme';
@@ -6,6 +6,10 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import RegisterScreen from './screens/RegisterScreen';
 import LoginScreen from './screens/LoginScreen';
 import { HomeScreen } from './screens/HomeScreen';
+import AddBallScreen from './screens/AddBallScreen';
+import UserListScreen from './screens/UserListScreen';
+import BallListScreen from './screens/BallListScreen';
+import UpdateProfileScreen from './screens/UpdateProfileScreen';
 
 const App = () => {
   return (
@@ -16,6 +20,10 @@ const App = () => {
           <Route path="/" component={HomeScreen} exact />
           <Route path="/register" component={RegisterScreen} />
           <Route path="/login" component={LoginScreen} />
+          <Route path="/admin/ball" component={AddBallScreen} />
+          <Route path="/admin/userList" component={UserListScreen} />
+          <Route path="/admin/ballList" component={BallListScreen} />
+          <Route path="/profile" component={UpdateProfileScreen} />
         </main>
       </ThemeProvider>
     </Router>
